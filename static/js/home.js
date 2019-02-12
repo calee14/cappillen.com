d7bfdb3c89f50d0afe21b15b999b3c7fc1ac099e
calee14 <caplee1000@gmail.com>
2019-02-11 21:46:46 -0800

    adding js to show and hide navbar

--- static/js/home.js
+++ static/js/home.js
@@ -9,3 +9,25 @@ $("#projects-button").click(function() {
         scrollTop: $("#projects").offset().top
     }, 2000);
 });
+(function ($) {
+  $(document).ready(function(){
+    
+	// hide .navbar first
+	$(".navbar").hide();
+	
+	// fade in .navbar
+	$(function () {
+		$(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+			if ($(this).scrollTop() > 100) {
+				$('.navbar').fadeIn();
+			} else {
+				$('.navbar').fadeOut();
+			}
+		});
+
+	
+	});
+
+});
+	}(jQuery));
