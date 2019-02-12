4b3128c1b3674ee0bc9e2a9d5f41d319b8baf1b2
calee14 <caplee1000@gmail.com>
2019-02-11 18:53:14 -0800

    adding js to scroll down

--- static/js/home.js
+++ static/js/home.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+$("#about-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+});
+$("#projects-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#projects").offset().top
+    }, 2000);
+});
