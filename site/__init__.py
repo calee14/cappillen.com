aa8028009f5058bb55f14f160ca5fad0f734db97
calee14 <caplee1000@gmail.com>
2020-09-17 17:11:01 -0700

    add site

---
+++ site/__init__.py
@@ -0,0 +1,19 @@
+# Import Flask for flask app object
+from flask import Flask
+from config import Config
+
+# import sentry_sdk
+# from sentry_sdk.integrations.flask import FlaskIntegration
+
+# sentry_sdk.init(
+#     dsn="https://3fcef6e888f047bd9f52298a7e13b729@o424091.ingest.sentry.io/5355434",
+#     integrations=[FlaskIntegration()]
+# )
+
+# Create flask app object
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+# Import all views
+import site.views
---
+++ site/views.py
@@ -0,0 +1,12 @@
+from certhasher import app
+from flask import render_template, make_response, url_for, send_file, abort, flash, request, redirect
+import numpy as np
+import os
+
+@app.route('/')
+def home():
+    render_template('home')
+
+@app.route('/debug-sentry')
+def trigger_error():
+    division_by_zero = 1 / 0
