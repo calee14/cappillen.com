eb368ab7f15d5aa6bce42065f0936a5d5cef45b8
calee14 <caplee1000@gmail.com>
2021-09-22 23:03:32 -0700

    add signin code with jwt

--- personalsite/views.py
+++ personalsite/views.py
@@ -1,5 +1,5 @@
-from personalsite import app
-from flask import render_template, make_response, url_for, send_file, abort, flash, request, redirect
+from personalsite import app, jwt
+from flask import render_template, make_response, url_for, send_file, abort, flash, request, redirect, jsonify
 from flask_jwt_extended import create_access_token, get_jwt, jwt_required
 import os
 import secret
@@ -10,18 +10,26 @@ def home():
 
 @app.route('/signin')
 def signin():
-    return render_template('signin')
+    if request.method == 'GET':
+        return render_template('signin.html')
+    if request.method == 'POST':
+        username = request.json.get('username', None)
+        password = request.json.get('password', None)
+        if username != secret.USERNAME or password != secret.PASSWORD:
+            return 'incorrect username'
+
+        additional_claims = {'aud':'some audience', 'hello':'there'}
+        access_token = create_access_token(username, additional_claims=additional_claims)
+        return jsonify(access_token=access_token)
+        
 
 @app.route('/dablog')
 @jwt_required
 def blog():
-    return render_template('bloglist')
+    data_claim = get_jwt()
+    return render_template('bloglist.html')
 
 @app.route('/dablog')
 @jwt_required
 def blogindv():
-    return render_template('blogidnv')
-
-@app.route('/debug-sentry')
-def trigger_error():
-    division_by_zero = 1 / 0
+    return render_template('blogidnv.html')
