c7f8e941ece57aa3b08578c16e776aec3a528c55
calee14 <caplee1000@gmail.com>
2021-09-23 23:43:01 -0700

    changing routes and add logout plan

--- personalsite/views.py
+++ personalsite/views.py
@@ -22,7 +22,7 @@ def signin():
         access_token = create_access_token(username, additional_claims=additional_claims)
         return jsonify(access_token=access_token)
         
-@app.route('/dablog', methods=['GET'])
+@app.route('/dastoryhub', methods=['GET'])
 @jwt_required   # https://flask-jwt-extended.readthedocs.io/en/stable/_modules/flask_jwt_extended/view_decorators/#jwt_required
                 # The wrap() provided by python is good for defining wrapper functions. helps preserve the meta data (__name__) of the original function.
 def blog():
@@ -30,8 +30,14 @@ def blog():
     return jsonify(hello=data_claim['hello'])
     return render_template('bloglist.html')
 
-@app.route('/dablog', methods=['GET'])
+@app.route('/dastory', methods=['GET'])
 @jwt_required
 def blogindv():
     story_num = request.args.get('story', default=1, type=int)
     return render_template('blogidnv.html')
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    # https://flask-jwt-extended.readthedocs.io/en/stable/refreshing_tokens/
+    # for removing tokens
+    return 'logging out sike'
