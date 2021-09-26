98a471c0b0fe17ba2a49df1b5981c7b043b3893a
calee14 <caplee1000@gmail.com>
2021-09-26 16:04:05 -0700

    complete sign in process

--- personalsite/views.py
+++ personalsite/views.py
@@ -13,17 +13,18 @@ def signin():
     if request.method == 'GET':
         return render_template('signin.html')
     if request.method == 'POST':
-        username = request.json.get('username', None)
-        password = request.json.get('password', None)
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
         if username != secret.USERNAME or password != secret.PASSWORD:
             return 'incorrect username'
 
         additional_claims = {'aud':'some audience', 'hello':'there'}
         access_token = create_access_token(username, additional_claims=additional_claims)
-        return jsonify(access_token=access_token)
+        return redirect(render_template('bloglist.html'))
         
 @app.route('/dastoryhub', methods=['GET'])
-@jwt_required   # https://flask-jwt-extended.readthedocs.io/en/stable/_modules/flask_jwt_extended/view_decorators/#jwt_required
+@jwt_required()   # https://flask-jwt-extended.readthedocs.io/en/stable/_modules/flask_jwt_extended/view_decorators/#jwt_required
                 # The wrap() provided by python is good for defining wrapper functions. helps preserve the meta data (__name__) of the original function.
 def blog():
     data_claim = get_jwt()
@@ -31,7 +32,7 @@ def blog():
     return render_template('bloglist.html')
 
 @app.route('/dastory', methods=['GET'])
-@jwt_required
+@jwt_required()
 def blogindv():
     story_num = request.args.get('story', default=1, type=int)
     return render_template('blogindividual.html')
