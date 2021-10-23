5ba23ed75136956263cf9648f856d5c105d514f4
calee14 <caplee1000@gmail.com>
2021-10-23 02:08:00 -0700

    manage key var

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
-from secret import JWT_SECRET_KEY
+JWT_SECRET_KEY = ''
+try:
+    from secret import JWT_SECRET_KEY
+except:
+    JWT_SECRET_KEY = os.environ['JWTKEY']
 class Config(object):
     TESTING = False
     DEBUG = True
