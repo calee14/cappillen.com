fa2a7fcd38eb92dcad57689b2c1cec239f09fd2f
calee14 <caplee1000@gmail.com>
2022-05-04 00:12:14 -0700

    unset cookies if expired token

--- personalsite/views.py
+++ personalsite/views.py
@@ -172,7 +172,7 @@ def custom_unauthorized_response(_err):
 @jwt.expired_token_loader
 def my_expired_token_callback(jwt_header, jwt_payload):
     res = make_response(redirect(url_for('signin')))
-    # unset_jwt_cookies(res)
+    unset_jwt_cookies(res)
     return res
 
 @app.route('/c2bd', methods=['GET'])
