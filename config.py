4c188a62ac2666110cef4fb6a2c84267cf0694fa
Cappillen Lee <cappillen@ucsb.edu>
2025-10-01 16:48:56 -0700

    update: redesign bio and site

--- config.py
+++ config.py
@@ -1,21 +1,23 @@
 import os
 import datetime
 
-JWT_SECRET_KEY = ''
+JWT_SECRET_KEY = ""
 try:
     from secret import JWT_SECRET_KEY
 except:
-    JWT_SECRET_KEY = os.environ['JWTKEY']
+    JWT_SECRET_KEY = os.environ["JWTKEY"]
+
+
 class Config(object):
     TESTING = False
     DEBUG = True
     SECRET_KEY = os.urandom(32)
     JWT_SECRET_KEY = JWT_SECRET_KEY
-    JWT_SESSION_COOKIE=False
-    JWT_TOKEN_LOCATION = ['cookies']
+    JWT_SESSION_COOKIE = False
+    JWT_TOKEN_LOCATION = ["cookies"]
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=30)
     JWT_COOKIE_SECURE = False
     JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(days=30)
-    JWT_COOKIE_CSRF_PROTECT = False 
+    JWT_COOKIE_CSRF_PROTECT = False
     JWT_ACCESS_CSRF_HEADER_NAME = "X-CSRF-TOKEN-ACCESS"
     JWT_REFRESH_CSRF_HEADER_NAME = "X-CSRF-TOKEN-REFRESH"
